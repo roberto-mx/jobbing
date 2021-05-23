1607359d0bafe8919734895869b8b90a37ec80ad
Roberto Gutiérrez García <roberto.gg.mx@gmail.com>
2021-05-23 01:19:47 -0500

    Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn jobbing:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -11,10 +11,12 @@ app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'Aprende tu mismo API'}, pythonic_params=True)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jce.sqlite3'
-app.debug = True
-app.run(port=8080)
+#app.debug = True
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
+if __name__ == '__main__':
+    app.run()
